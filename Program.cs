8b0c528bc59c9879f5323e44c547f2f98bce4eab
Kanchitfrank <kanfrankchot@gmail.com>
2023-03-02 10:13:46 +0700

    add input

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework1{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.Write("Password : "); int password = int.Parse(Console.ReadLine());
+            Console.Write("Agency : "); string agency = (Console.ReadLine());
+            if(agency == "CIA"){
+                Console.WriteLine("Hello");
+            }else{
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
