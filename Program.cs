781885cc7d8da8a8d17f3ec890c3aa12144239be
Kanchitfrank <kanfrankchot@gmail.com>
2023-03-02 10:25:58 +0700

    Complete CIA

--- Program.cs
+++ Program.cs
@@ -6,8 +6,19 @@ namespace Homework1{
         static void Main(string[] args){
             Console.Write("Password : "); int password = int.Parse(Console.ReadLine());
             Console.Write("Agency : "); string agency = (Console.ReadLine());
+
+            float ciadigit = password % 10;
+            float ciatenno = password % 100; int ciaten = (int)ciatenno / 10;  
+            float ciathousandsno = password % 10000; int ciathousands = (int)ciathousandsno / 1000;
+            Console.Write("{0} {1} {2}",ciadigit, ciaten, ciathousands);
             if(agency == "CIA"){
-                Console.WriteLine("Hello");
+               if(ciadigit % 3 == 0 
+                && ciaten != 1 && ciaten != 3 && ciaten != 5 
+                && ciathousands > 6 && ciathousands != 8){
+                 Console.WriteLine("True");
+               }else{
+                Console.WriteLine("False");
+               }
             }else{
                 Console.WriteLine("False");
             }
