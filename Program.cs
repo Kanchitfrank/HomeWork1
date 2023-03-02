67543f827178d81763ec37766719820027440539
Kanchitfrank <kanfrankchot@gmail.com>
2023-03-02 11:03:55 +0700

    Complete FBI

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace Homework1{
             float ciadigit = password % 10;
             float ciatenno = password % 100; int ciaten = (int)ciatenno / 10;  
             float ciathousandsno = password % 10000; int ciathousands = (int)ciathousandsno / 1000;
-            Console.Write("{0} {1} {2}",ciadigit, ciaten, ciathousands);
+            // Console.WriteLine("{0} {1} {2}",ciadigit, ciaten, ciathousands);
+
+            int fbihundredthousands = password / 100000;
+            int fbihundredno = password % 1000; int fbihundred = fbihundredno /100;
+            int fbitenthousandsno = password / 10000 ; int fbitenthousands = fbitenthousandsno % 10; 
+            Console.WriteLine("{0} {1} {2}",fbihundred, fbitenthousands, fbihundredthousands);
+            
             if(agency == "CIA"){
                if(ciadigit % 3 == 0 
                 && ciaten != 1 && ciaten != 3 && ciaten != 5 
@@ -19,9 +25,15 @@ namespace Homework1{
                }else{
                 Console.WriteLine("False");
                }
-            }else{
-                Console.WriteLine("False");
+            }if(agency == "FBI"){
+               if(fbihundredthousands>=4 && fbihundredthousands <=7 
+                && fbihundred != 6  && fbihundred % 2 == 0
+                && fbitenthousands % 2 !=0){
+                    Console.WriteLine("True");
+                }else{
+                    Console.WriteLine("False");
+                }
+                     }
             }
         }
     }
-}
